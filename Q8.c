3284e7faf6bc1cd249920aedf0d305ca319193a0
techieydv <priyanka25csu307@ncuindia.edu>
2025-11-02 11:14:00 +0530

    Add files via upload

---
+++ Q8.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    
+    int n, i;
+    long first = 0, second = 1, next;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= n; i++) {
+        printf("%lld", first);
+        if (i < n)
+            printf(", ");
+        next = first + second;
+        first = second;
+        second = next;
+    }
+
+    printf("\n");
+
+    return 0;
+}
